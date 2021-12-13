6556e7ff7bf0819de52325555410b246449d31cc
Jason Washburn <jason@shyftsolutions.io>
2021-12-12 18:01:26 -0600

    bugfix for typo in backend main

--- backend/main.py
+++ backend/main.py
@@ -31,4 +31,4 @@ def get_image(style: str, file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main.app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
